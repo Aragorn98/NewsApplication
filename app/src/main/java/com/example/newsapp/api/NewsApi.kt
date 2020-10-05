3ffc11ff5a3d88389a71c216476c1953caa8cfcc
Argyn <argynaitkazy@gmail.com>
2020-10-05 11:18:52 +0600

    updated api-key

--- app/src/main/java/com/example/newsapp/api/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/api/NewsApi.kt
@@ -7,9 +7,9 @@ import retrofit2.http.Query
 
 
 interface NewsApi {
-    @GET("top-headlines?q=apple&apiKey=867a584b6c6e4c5fbad52597553c390a")
+    @GET("top-headlines?q=apple&apiKey=a1fb4b6e45e847f6a2d3ea50f46843b0")
     suspend fun getTopHeadlines(@Query("page") pageNumber: Int): Response<News>
 
-    @GET("everything?q=apple&apiKey=867a584b6c6e4c5fbad52597553c390a")
+    @GET("everything?q=apple&apiKey=a1fb4b6e45e847f6a2d3ea50f46843b0")
     suspend fun getEverything(@Query("page") pageNumber: Int): Response<News>
 }
